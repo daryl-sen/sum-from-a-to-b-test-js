4d66aa39446ff9941d22eb4c757b151d0c6867ad
daryl <tsdaryl@gmail.com>
2021-01-08 17:39:13 -0800

    all checks passed

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+
+  // recursive case
+  if (fromN < toN -1) {
+    return fromN + sum(fromN + 1, toN);
+  } else if (fromN === toN) {
+    return fromN;
+  }
+
+  // base case
+  return fromN + toN;
+
 }
 
+// console.log(sum(3, 7));
+
 module.exports = sum;
+
+// 3, 7
+
+// (3 + (4 + (5 + (6 + 7))))
+
+
+
+
+
+
